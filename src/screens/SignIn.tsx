b012ccc010664ab37e3a0c8b4af63bcf687b81f8
joaoguibc <joaoguibc@gmail.com>
2022-07-23 23:42:24 -0300

    Feat: Start user login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
-import { Heading, Icon, VStack, useTheme } from "native-base";
+import { useState } from "react";
 import { Envelope, Key } from 'phosphor-react-native'
+import { Heading, Icon, VStack, useTheme } from "native-base";
 
 import Logo from '../assets/logo_primary.svg';
 
@@ -7,7 +8,16 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    if (!email || !password) return;
+
+    console.log(`E-mail: ${email} || Password: ${password}`)
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -21,6 +31,7 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         autoCapitalize="none"
+        onChangeText={setEmail}
         keyboardType="email-address"
         InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]} />} />}
       />
@@ -28,10 +39,11 @@ export function SignIn() {
         mb={8}
         secureTextEntry
         placeholder="Senha"
+        onChangeText={setPassword}
         InputLeftElement={<Icon ml={4} as={<Key color={colors.gray[300]} />} />}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   )
 }
